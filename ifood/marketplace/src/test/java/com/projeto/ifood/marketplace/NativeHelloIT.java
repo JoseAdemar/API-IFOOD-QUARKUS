24f3a4f6998f3dc8aae488aaf60ed97dd3ad55a1
jose_ademar <joseademar.junior@shift.com.br>
2021-11-27 12:16:24 -0300

    Implementado os endpoints de prato e restaurante

---
+++ ifood/marketplace/src/test/java/com/projeto/ifood/marketplace/NativeHelloIT.java
@@ -0,0 +1,9 @@
+package com.projeto.ifood.marketplace;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloIT extends HelloTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ ifood/pedido/src/test/java/com/projeto/ifood/pedido/NativeHelloIT.java
@@ -0,0 +1,9 @@
+package com.projeto.ifood.pedido;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloIT extends HelloTest {
+
+    // Execute the same tests but in native mode.
+}
