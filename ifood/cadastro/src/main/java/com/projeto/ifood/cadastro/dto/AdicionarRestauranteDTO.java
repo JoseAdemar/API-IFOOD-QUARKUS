a6e4a0061a088e7d21b0c6d7c430eb2d23abe36e
jose_ademar <joseademarti@gmail.com>
2022-01-08 20:39:52 -0300

    Trabalhando com Hibernate Validator

--- ifood/cadastro/src/main/java/com/projeto/ifood/cadastro/dto/AdicionarRestauranteDTO.java
+++ ifood/cadastro/src/main/java/com/projeto/ifood/cadastro/dto/AdicionarRestauranteDTO.java
@@ -1,11 +1,20 @@
 package com.projeto.ifood.cadastro.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class AdicionarRestauranteDTO {
 	
+	 @NotNull
+	 @NotEmpty
 	 public String propietario;
 	 
+	 @NotNull
+	 @NotEmpty
 	 public String cnpj;
 	 
+	 @NotNull
+	 @NotEmpty
 	 public String nome;
 	 
 	 public LocalizacaoDTO localizacao;
